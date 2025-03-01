ae0b098e17b071ca460e2200ba49b161dd7dd462
martineap <meap_7@hotmail.com>
2025-03-01 11:53:56 -0600

    Create Counter.js
    Thsi is the code I create for my submission project of IP2 of counter. 
    
    The buttons are created in the canvas, but something is wrong with the button event listener that when I click it, the value is not passed to the counterDisplay

---
+++ Counter.js
@@ -0,0 +1,52 @@
+function counter(){
+    
+    this.name = "counter";
+    this.icon = "assets/count.png";
+        
+    var self = this;
+    var conteo = 0;
+    var counterDisplay;
+    var plusBtn;
+    var minusBtm;
+
+    this.populateOptions = function() {
+        
+    select("#content").html("<h3 id='counter'>0</h3><button id='plusBtn'>+</button><button id='minusBtn'>-</button>");
+    
+     counterDisplay = document.querySelector('#counter');
+     plusBtn = document.querySelector('#plusBtn');
+     minusBtn = document.querySelector('#minusBtn');
+        
+    }
+    
+    this.updateCounter = function(value){
+        conteo = conteo + value;
+        counterDisplay.textContent = conteo;
+        
+         plusBtn.addEventListener('click',function(){
+        updateCounter(1);
+    });
+    minusBtn.addEventListener('click',function(){
+        updateCounter(1);
+    });   
+        
+        if(conteo >= 10){
+            plusBtn.setAttribute('disabled', true);
+        } else{
+            plusBtn.setAttribute('disabled',false);
+        }
+        
+        if(counter<=0){
+            minusBtn.setAttribute('disabled',true);
+        } else{
+            minusBtn.setAttribute('disabled',false);
+        } 
+    }   
+    
+    this.draw =function(){
+        
+                        }
+    this.unselectTool = function() {
+		select("#content").html("");
+	};
+}
